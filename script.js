86e3c16e1511d67e6ba6485e6c4247a2d3350b95
globalwavelogis <134814738+globalwavelogis@users.noreply.github.com>
2023-05-31 08:02:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Generate random tracking details
 function generateTrackingDetails(trackingNumber) {
   var trackingDetails = [
-    { trackingNumber: 'LON321654789CZE', status: 'In Transit', location: 'Mediterranean', date: '2023-05-30', time: '14:25' },
-    { trackingNumber: 'LON221654799CZE', status: 'Arrived', location: 'Los Angeles', date: '2023-06-02', time: '09:10' },
+    { trackingNumber: 'LON321612789CZE', status: 'V průběhu přepravy', location: 'Mediterranean, Lon', date: '2023-05-31', time: '9:25' },
+    { trackingNumber: 'LON201654709CZE', status: 'V průběhu přepravy', location: 'Mediterranean, Lon, Seaport 1', date: '2023-05-31', time: '09:15' },
     { trackingNumber: 'LON324654557CZE', status: 'Out for Delivery', location: 'San Francisco', date: '2023-06-04', time: '11:45' },
     { trackingNumber: 'LON123654987CZE', status: 'Delivered', location: 'San Francisco', date: '2023-06-04', time: '14:30' }
   ];
